b11ca4c49fbfb6e2c30c7bb9aa0ed7a41ff27eb2
rothej <21348884+rothej@users.noreply.github.com>
2026-03-27 07:46:08 +0000

    Deploying to gh-pages from @ rothej/rothej-portfolio@ed9f260ed66b6cfcea183752d9637374e4e210b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/2029-03-28-setting-up-a-vaultwarden-password-manager-on-a-debian-server/";
-          
-        },
-      },{id: "post-xilinx-amd-vivado-soc-fpga-development-and-debug-workflow",
+        },{id: "post-xilinx-amd-vivado-soc-fpga-development-and-debug-workflow",
         
           title: "Xilinx/AMD Vivado SoC FPGA Development and Debug Workflow",
         
